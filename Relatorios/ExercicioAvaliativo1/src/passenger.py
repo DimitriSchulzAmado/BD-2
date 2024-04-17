7d466dcbfbc8f16502dc60465fee63fbab7f1c95
Dimitri Schulz Amado <81584801+DimitriSchulzAmado@users.noreply.github.com>
2024-04-16 23:44:09 -0300

    :sparkles: feat: Driver System
    In this exercise we created a simple system of drivers per application.

---
+++ Relatorios/ExercicioAvaliativo1/src/passenger.py
@@ -0,0 +1,13 @@
+from src.base import BaseEntity
+
+
+class Passenger(BaseEntity):
+    def __init__(self, name, document):
+        self.name = name
+        self.document = document
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "document": self.document
+        }
